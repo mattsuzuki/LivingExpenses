527dfa87319eeef5a55dcc5f0c26cad3b73b8583
mattsuzuki <matthewjamessuzuki@gmail.com>
2022-08-12 10:35:29 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,66 @@
+// Dependencies
+var express = require("express");
+var bodyParser = require("body-parser");
+
+// Create app
+var app = express();
+
+// Configure app
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
+app.use(bodyParser.json());
+
+// Add routes
+app.get("/", function (req, res) {
+  res.render("index");
+});
+
+app.post("/calculate", function (req, res) {
+  const {
+    currentMonthlySalary,
+    yearsAtCurrentJob,
+    currentLivingExpenses,
+    currentTimeSpentWorking,
+    expectedMonthlySalary,
+  } = req.body;
+
+  // Calculate the results
+  var currentMoneyRaw = currentMonthlySalary * (yearsAtCurrentJob * 12); // 3600000
+  var softwareMoneyRaw = expectedMonthlySalary * (yearsAtCurrentJob * 12);
+  var jobMoneySavedRaw =
+    (currentMonthlySalary * 12 - currentLivingExpenses * 12) *
+    yearsAtCurrentJob;
+  var softwareMoneySavedRaw =
+    (expectedMonthlySalary * 12 - currentLivingExpenses * 12) *
+    yearsAtCurrentJob;
+
+  // Format money
+  var formatter = new Intl.NumberFormat("en-US", {
+    style: "currency",
+    currency: "USD",
+  });
+
+  var currentMoney = formatter.format(currentMoneyRaw);
+  var softwareMoney = formatter.format(softwareMoneyRaw);
+  var difference = formatter.format(softwareMoneyRaw - currentMoneyRaw);
+  var jobMoneySaved = formatter.format(jobMoneySavedRaw);
+  var softwareMoneySaved = formatter.format(softwareMoneySavedRaw);
+
+  // Pass and render view
+  res.render("results", {
+    yearsAtCurrentJob,
+    currentMoney,
+    softwareMoney,
+    softwareMoneySaved,
+    jobMoneySaved,
+    difference,
+  });
+});
+
+// Start server
+app.listen(3000);
